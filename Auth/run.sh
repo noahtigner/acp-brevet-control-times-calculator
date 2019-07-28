8b5417035209a50c4fc63a6780d0b53f6f1988d1
Noah Tigner <nzt@cs.uoregon.edu>
2019-07-28 13:46:24 -0700

    Refactored

--- Auth/run.sh
+++
@@ -1,4 +0,0 @@
-docker stop auth_website_1 auth_laptop-service_1 auth_mongodb_1
-docker rm auth_website_1 auth_laptop-service_1 auth_mongodb_1
-docker image rm auth_laptop-service demo flask-sample-on 8ba7cc1c8357
-docker-compose up
